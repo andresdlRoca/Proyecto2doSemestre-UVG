d3c44d601f6a4dfc7ffb2d5b4ba77da47376888b
jwlh00 <60154594+jwlh00@users.noreply.github.com>
2020-09-10 15:11:46 -0600

    Add files via upload

---
+++ Codigo e implementacion/logIn.java
@@ -0,0 +1,97 @@
+import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Random;
+import java.util.ArrayList;
+
+
+class logIn{
+	Scanner scan = new Scanner(System.in);
+	boolean usernameGate = true;
+	String username;
+	String password;
+	boolean passwordGate = true;
+	int choiceAccount;
+	String name;
+	String address;
+	String age;
+	String email;
+
+
+
+	
+	HashMap<String, ArrayList<String>> dataBank = new HashMap<String, ArrayList<String>>();
+
+	public void getLogIn(){
+		usernameGate = true;
+
+		
+
+		while(usernameGate){
+			System.out.println("Bienvenido a nuestro programa, porfavor ingrese su usuario para continuar.");
+			username = scan.next();
+			passwordGate = true;
+
+			if(dataBank.containsKey(username)){
+				
+				while(passwordGate){
+					System.out.println("Ingrese su contraseña porfavor");
+					password = scan.next();
+					if(dataBank.get(username).contains(password)){
+					System.out.println("Seccion ingresada correctamente.");
+					passwordGate = false;
+					usernameGate = false;
+					}else{
+						System.out.println("Contrasena ingresada incorrecta, ingrese otra vez.");
+						
+					}
+				}
+			}else{
+				System.out.println("Ese usuario no esta registrado en nuestra base de datos, Desea crear una cuenta nueva?\n1.Si\n2.No");
+				choiceAccount = scan.nextInt();
+
+				if(choiceAccount == 2){
+					System.out.println("");
+				}else{
+
+					System.out.println("Ingrese su contraseña");
+					password = scan.next();
+					
+
+					System.out.println("Ingrese su nombre porfavor");
+					name = scan.next();
+					
+
+					System.out.println("Ingrese su vivienda");
+					address = scan.next();
+						
+
+					System.out.println("Ingrese su edad");
+					age = scan.next();
+					
+
+					System.out.println("Ingrese su correo electronico");
+					email = scan.next();
+					
+
+
+
+					dataBank.put(username, new ArrayList<String>());
+					dataBank.get(username).add(password);
+					dataBank.get(username).add(name);
+					dataBank.get(username).add(address);
+					dataBank.get(username).add(age);
+					dataBank.get(username).add(email);
+
+					System.out.println(dataBank);
+
+					
+					usernameGate = false;
+
+				}
+				
+			}
+
+		}
+		
+	}
+}
