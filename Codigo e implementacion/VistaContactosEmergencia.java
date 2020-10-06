be44152be91b2ac2e15f8404252386a6c54cad6b
Andres de la Roca <andresdelaroca11@gmail.com>
2020-10-05 19:09:55 -0600

    Creacion de clase vista

---
+++ Codigo e implementacion/VistaContactosEmergencia.java
@@ -0,0 +1,11 @@
+public class VistaContactosEmergencia {
+    
+    public void mostrarContactos(String contactos) {
+        System.out.println("Los contactos de emergencia elegidos son: \n" + contactos);
+    }
+
+    public void mostrarError() {
+        System.out.println("Se ha ingresado una opcion invalida intentalo de nuevo");
+    }
+
+}
