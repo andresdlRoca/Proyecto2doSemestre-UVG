b851541a0898d3960d04b7866f5a32916880e11f
Andres de la Roca <andresdelaroca11@gmail.com>
2020-10-05 19:08:57 -0600

    Creacion de clase controladora

---
+++ Codigo e implementacion/ControladorContactosEmergencia.java
@@ -0,0 +1,42 @@
+/******************************************************************
+ControladorContactosEmergencia.java
+Integrantes: 
+Jun Woo Lee Hong 
+Cristian Eduardo Aguirre Duarte 
+Andrés Alejandro de la Roca Pineda 
+Sayra Estefanía Elvira Ramos 
+Pablo Daniel Gonzalez Ramos 
+Manuel Alejandro Archila Moran
+
+Última modificación: 10/9/2020
+
+Clase que controla la clase modelo y vision de los contactos de emergencia,
+se le pregunta alusuario la region en la que desea ver los contactos 
+de emergencia y se le muestra la informacion correspondiente.
+******************************************************************/
+public class ControladorContactosEmergencia {
+    
+    private ModeloContactosEmergencia contactosModel;
+    private VistaContactosEmergencia contactosVista;
+
+    public ControladorContactosEmergencia(ModeloContactosEmergencia contactosModel, VistaContactosEmergencia contactosVista) {
+        this.contactosModel = contactosModel;
+        this.contactosVista = contactosVista;
+    }
+
+    //Metodo que inicializa los hashmaps que guardan los contactos de emergencia
+    public void inicializarContactos() {
+        contactosModel.crearListaContactos();
+    }
+
+    //Metodo que busca los contactos y se los muestra al usuario.
+    public void buscarContactos(int indice) {
+        if (indice <= 0 || indice >= 3) {
+            contactosVista.mostrarError();
+        } else {
+            contactosVista.mostrarContactos(contactosModel.getContacto(indice));
+        }
+        
+    }
+    
+}
